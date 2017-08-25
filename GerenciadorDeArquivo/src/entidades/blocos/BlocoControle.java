3c8d63cbdb3bcd5db2a8826ac30eaf6a23a511fd
Thiago Maia <thiago.maia971@gmail.com>
2017-08-25 18:57:46 -0300

    Implementando o BlocoControle

--- GerenciadorDeArquivo/src/entidades/blocos/BlocoControle.java
+++ GerenciadorDeArquivo/src/entidades/blocos/BlocoControle.java
@@ -1,9 +1,29 @@
 package entidades.blocos;
 
+import interfaces.IBinary;
+
 /**
  * Created by José Victor on 09/08/2017.
  */
-public class BlocoControle {
+public class BlocoControle implements IBinary{
     private BlocoContainerHeader blocoHeader;
     private BlocoDado[] blocoDados;
+
+    public BlocoControle(int containerId) {
+        this.blocoHeader = new BlocoContainerHeader(containerId);
+    }
+
+    public BlocoContainerHeader getHeader() {
+        return blocoHeader;
+    }
+
+    @Override
+    public byte[] toByteArray() {
+        return new byte[0];
+    }
+
+    @Override
+    public <T> T fromByteArray(byte[] byteArray) {
+        return null;
+    }
 }
