af8db32815d6dfc7bf9d7abadb3c04215db0fd02
Thiago Maia <thiago.maia971@gmail.com>
2017-08-25 18:57:15 -0300

    Implementando BlocoContainer
    Começando a implementear o BlocoContainer.
    Implmentação para retornar o ContainerId desse bloco.

--- GerenciadorDeArquivo/src/entidades/blocos/BlocoContainer.java
+++ GerenciadorDeArquivo/src/entidades/blocos/BlocoContainer.java
@@ -1,11 +1,30 @@
 package entidades.blocos;
 
-public class BlocoContainer {
+import interfaces.IBinary;
+
+public class BlocoContainer implements IBinary {
     private BlocoControle blocoControle;
     private BlocoDado[] blocosDados;
 
-    public int getContainerId () {
-        return 1;
+    public BlocoContainer(int containerId) {
+        this.blocoControle = new BlocoControle(containerId);
     }
 
+    public int getContainerId () {
+        return this.blocoControle.getHeader().getContainerId();
+    }
+
+    public BlocoControle getBlocoControle() {
+        return blocoControle;
+    }
+
+    @Override
+    public byte[] toByteArray() {
+        return new byte[0];
+    }
+
+    @Override
+    public <T> T fromByteArray(byte[] byteArray) {
+        return null;
+    }
 }
